702099ed07d586ce23c103c0995e2b19b5d840e2
Nick Lima <nlima446@gmail.com>
2019-03-20 14:24:32 -0400

    first commit

---
+++ import.js
@@ -0,0 +1,137 @@
+const scraper = require("./scraper");
+
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/drive.file', 
+'https://www.googleapis.com/auth/drive.readonly',
+'https://www.googleapis.com/auth/documents.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+
+// Load client secrets from a local file.
+fs.readFile('credentials.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Google Drive API.
+  authorize(JSON.parse(content), importDoc);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.web;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getAccessToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getAccessToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+function importDoc(auth) {	
+	url = process.argv.slice(2)[0]
+	console.log('url: ' + url)
+	var tab, artist, song_name, raw_tabs;
+	scraper.getSong(url, function() {
+		tab = arguments[0];
+		artist = tab.artist;
+		song_name = tab.song_name;
+		raw_tabs = tab.raw_tabs.replace(/(\[ch\]|\[\/ch\])/g, '');
+	    console.log('Importing: ' + song_name + ' by ' + artist);
+	    console.log(raw_tabs)
+		
+		const docs = google.docs({version: 'v1', auth});
+		const drive = google.drive({version: 'v3', auth})
+		var copyId;
+		drive.files.copy({
+			'fileId': '1K7dvZpTODZcfwxcLEsFJVCJhxaaO0_HfZBUn5rKcE3M',
+			'resource': { 'name': '[DRAFT] ' + song_name + ' - ' + artist}
+		}, (err, res) => {
+			if (err) return console.log('The API returned an error: ' + err);
+			copyId = res.data.id;
+			console.log('Copy id: ' + copyId)
+			docs.documents.get({
+				'documentId': copyId,
+			}, (err, res) => {
+				if (err) return console.log('The API returned an error: ' + err);
+				// console.log(res.data.body.content)
+				const requests = [{
+						'replaceAllText': { 
+							'replaceText' : artist,
+							'containsText': {
+								'text' : '_Artist_',
+								'matchCase' : true
+							}
+						}
+					},{
+						'replaceAllText': { 
+							'replaceText' : song_name,
+							'containsText': {
+								'text' : '_Song_',
+								'matchCase' : true
+							}
+						} 
+					}, {					
+						'replaceAllText': { 
+							'replaceText' : raw_tabs,
+							'containsText': {
+								'text' : '_Content_',
+								'matchCase' : true
+							}
+						}
+					}]
+				docs.documents.batchUpdate({
+					'documentId': copyId,
+					'resource' : { 
+						'requests': requests 
+					}
+				}, (err, res) => {
+					if (err) return console.log('The API returned an error: ' + err);
+					// console.log(res)
+				})
+			});
+		})
+	});	
+}
