182520d49d1faf9542ab4a53c17e7013387a87f0
Nick Lima <nlima446@gmail.com>
2019-12-14 23:52:17 -0800

    idk

--- auth.js
+++ auth.js
@@ -8,17 +8,24 @@ module.exports = (passport) => {
     passport.deserializeUser((user, done) => {
         done(null, user);
     });
+    fs.readFile('credentials.json', (err, content) => {
+        if (err) 
+            console.log('Error loading client secret file:', err);
+        // console.log(JSON.parse(content))
+        creds = JSON.parse(content)
+        passport.use(new GoogleDriveStrategy({
+            clientID: process.env.client_id || creds.web.client_id,
+            clientSecret: process.env.client_secret|| creds.web.client_secret,
+            callbackURL: 'https://guitartabimporter.herokuapp.com/import/auth/google/callback' || creds.web.redirect_uris[0]
+        }, 
+        (token, refresh_token, profile, done) => {
+            user = {};
+            user.token = token;
+            user.refresh_token = refresh_token;
+            user.profile = profile;
+            return done(null, user)
+        }));
+    });
 
-    passport.use(new GoogleDriveStrategy({
-        clientID: process.env.client_id,
-        clientSecret: process.env.client_secret,
-        callbackURL: 'https://guitartabimporter.herokuapp.com/import/auth/google/callback'
-    }, 
-    (token, refresh_token, profile, done) => {
-        user = {};
-        user.token = token;
-        user.refresh_token = refresh_token;
-        user.profile = profile;
-        return done(null, user)
-    }));
+    
 };
