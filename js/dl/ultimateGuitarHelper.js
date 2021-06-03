9ac70575112502c51edd3c7055db7e45c6f8d6e3
Nick Lima <nlima446@gmail.com>
2021-06-02 21:27:57 -0700

    converted to react

--- js/dl/ultimateGuitarHelper.js
+++ js/dl/ultimateGuitarHelper.js
@@ -4,7 +4,11 @@ async function getSong( url ) {
 	const browser = await pupp.launch({
 	  args: [
 	    '--no-sandbox',
-	    '--disable-setuid-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-infobars',
+      '--window-position=0,0',
+      '--ignore-certifcate-errors',
+      '--ignore-certifcate-errors-spki-list',
       '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'
 	  ],
 	});
