b7fec03092e61672e6701ca2c68927c2beb8287d
Nick Lima <nlima446@gmail.com>
2026-03-20 13:24:36 -0400

    fixed issue when loading library page

--- app/library/page.js
+++ app/library/page.js
@@ -68,7 +68,7 @@ function EditModal({ opened, close, tab, saveTab, isNewTab }) {
 					<TextInput label="Artist Name" key={form.key('artistName')} {...form.getInputProps('artistName')} />
 					<Select label="Tuning" key={form.key('tuning')} {...form.getInputProps('tuning')} data={['EADGBe', 'EbAbDbGbBbeb', 'DADGBe', 'DADF#AD']} />
 					<NumberInput label="Capo" key={form.key('capo')} {...form.getInputProps('capo')} />
-					<Checkbox label="Draft" defaultChecked={('draft' in tab) ? tab['draft'] : true} key={form.key('draft')} {...form.getInputProps('draft')} />
+					<Checkbox label="Draft" defaultChecked={(tab && 'draft' in tab) ? tab['draft'] : true} key={form.key('draft')} {...form.getInputProps('draft')} />
 					<Group justify="flex-end" mt="md">
 						<Button type='submit'>Save</Button>
 					</Group>
